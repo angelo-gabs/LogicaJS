6006a559001f93f41517f350061b19051a5b78c8
Angelo Gabriel <angelogcsilveira@gmail.com>
2023-03-22 15:05:51 -0300

    Exercicio 11

---
+++ 11-parOuImpar.js
@@ -0,0 +1,3 @@
+//Elaborar um algoritmo que leia um número e informe se ele é para ou ímpar
+let num = 12;
+num%2 == 0 ? console.log(`O número ${num} é par.`) : console.log(`O número ${num} é ímpar.`);
